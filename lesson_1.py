af539b83968108e96e558a5e71c52d3be920e8db
Kate <katerina.ev@gmail.com>
2023-11-02 20:48:17 +0400

    save lesson_1.py in git

---
+++ lesson_1.py
@@ -0,0 +1,38 @@
+# 1
+
+# print(17 / 2 * 3 + 2)
+#
+# print(2 + 17 / 2 * 3)
+#
+# print(19 % 4 + 15 / 2 * 3)
+#
+# print(15 + 6 - 10 * 4)
+#
+# print(17 / 2 % 2 * 3 ** 3)
+#
+# # 2
+#
+# print(17 / 2 * (3 + 2))
+#
+# print(2 + 17 / 2 * 3)
+#
+# print(19 % 4 + 15 / 2 * 3)
+#
+# print(15 + 6 - 10 * 4)
+#
+# print(17 / 2 % 2 * 3 ** 3)
+
+# # 3
+#
+# print(11 - 3 * 1.5)
+#
+# # 4
+#
+# ann_apple = 2
+# pol_apple = 2
+#
+# print('Ann has', ann_apple, 'apples. ' "Pol has", pol_apple, 'apples.')
+
+
+
+
